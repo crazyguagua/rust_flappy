78a1ac396c9019f1d8c9084ab0307e65278c215a
xiaodongzhang <saoziasdf1234@sina.com>
2024-03-18 20:50:04 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use bracket_lib::prelude::*;
 
 enum GameMode{
@@ -15,14 +16,18 @@ const FRAME_DURATION:i32 = 75;
 struct  State{
     mode:GameMode,
     player:Player,
-    frame_time:f32
+    frame_time:f32,
+    score:i32,
+    obstacle:Obstacle, //障碍物
 }
 impl State{
     fn new()->Self{
         State{
             mode:GameMode::Menu,
             player:Player::new(0,0),
-            frame_time:0.0
+            frame_time:0.0,
+            score:0,
+            obstacle:Obstacle::new(SCREEN_WIDTH,0)
         }
     }
     fn main_menu(&mut self, ctx:&mut BTerm){
@@ -63,13 +68,25 @@ impl State{
 
         ctx.print(0,0,"Press Space to Flap");
 
+        //打印得分
+        ctx.print(0,1,format!("Score :{}",self.score));
+
+        self.obstacle.render(ctx,self.player.x);
+
+        if self.player.x > self.obstacle.x{
+            self.score +=1;
+            //创建一个新的管子
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH,self.score);
+        }
+
+
         self.player.render(ctx);
 
         if let Some(VirtualKeyCode::Space) = ctx.key  {
             self.player.fly_up()
         }
-
-        if(self.player.y == SCREEN_HEIGHT){
+        //是否撞到管道
+        if(self.player.y == SCREEN_HEIGHT || self.obstacle.hit_bostacle(&self.player)){
             self.mode = GameMode::End
         }
 
@@ -105,17 +122,57 @@ impl Player{
           self.velocity += 0.4;
         }
         self.y = self.velocity as i32;
-        // self.x += 1;
+        self.x += 1;
         if(self.y<0){
             self.y = 0;
         }
     }
 
     fn fly_up(& mut self){
-        println!("{}",self.velocity);
-        if(self.y > 0){
+        if self.y > 0{
             self.velocity -= 0.8;
         }
+    }
+}
+
+
+struct Obstacle{
+    x:i32, //世界空间的横坐标
+    gap_y:i32,
+    size:i32
+}
+
+impl Obstacle{
+    fn new(x:i32,score:i32)->Self{
+        let mut random = RandomNumberGenerator::new();
+        Obstacle{
+            x,
+            gap_y:random.range(10,40),
+            size:i32::max(2,20-score)
+        }
+    }
+
+    fn render(&mut self,ctx:&mut BTerm,player_x:i32){
+        println!("obstacle render!! x:{}",self.x);
+        //屏幕空间
+        let screen_x = self.x - player_x;
+        let half_size:i32 = self.size /2;
+        for y in 0..self.gap_y - half_size{
+            ctx.set(screen_x,y,RED,BLACK,to_cp437('|'))
+        }
+        for y in self.gap_y + half_size..SCREEN_HEIGHT{
+            ctx.set(screen_x,y,RED,BLACK,to_cp437('|'))
+        }
+    }
+
+    //碰撞
+    fn hit_bostacle(&self,player:&Player)->bool{
+        let half_size = self.size/2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && ( player_above_gap || player_below_gap )
+
     }
 }
 
